69e4a4651382ce56e46ab8565448819f61115348
HuaXiaoji_Desktop <10121510098@stu.ecnu.edu.cn>
2025-05-23 02:20:21 +0800

    apply pixel theme

--- translation.js
+++ translation.js
@@ -1,24 +1,23 @@
 const translations = {
     'zh-CN': {
-        title: '欢迎访问！',
-        paragraph: '这是一个段落',
+        title: '杨天乐',
+        greeting: '欢迎来到我的个人主页！',
+        simpleIntroductionOfMyself: '我是<a href="https://math.ecnu.edu.cn/">华东师范大学数学科学学院</a>的一名本科生，研究方向为代数几何，特别是代数动力系统。'
     },
     'en-US': {
-        title: 'Welcome!',
-        paragraph: 'This is a paragraph',
+        title: 'Tianle Yang',
+        greeting: 'Welcome to my personal homepage!',
+        simpleIntroductionOfMyself: 'I am a undergraduate student from <a href="https://math.ecnu.edu.cn/">School of Mathematical Science, East China Normal University</a>.My academic interest is on algebraic geometry, especially algebraic dynamics.'
     }
 };
 
-// 检测浏览器语言的函数
-function detectBrowserLanguage() {
-    const userLang = navigator.language || navigator.userLanguage;
-    return userLang.startsWith('zh') ? 'zh-CN' : 'en-US';
-}
 // 更新语言的函数
 function updateContent() {
     document.getElementById('title').textContent = translations[currentLanguage].title;
-    document.getElementById('paragraph').textContent = translations[currentLanguage].paragraph;
+    document.getElementById('greeting').textContent = translations[currentLanguage].greeting;
+    document.getElementById('simpleIntroductionOfMyself').innerHTML = translations[currentLanguage].simpleIntroductionOfMyself;
 }
+
 // 切换到中文的函数
 function switchLanguageToChinese() {
     currentLanguage = 'zh-CN';
@@ -29,7 +28,11 @@ function switchLanguageToEnglish() {
     currentLanguage = 'en-US';
     updateContent();
 }
-
+// 检测浏览器语言的函数
+function detectBrowserLanguage() {
+    const userLang = navigator.language || navigator.userLanguage;
+    return userLang.startsWith('zh') ? 'zh-CN' : 'en-US';
+}
 
 // 初始化语言设置
 let currentLanguage = detectBrowserLanguage();
